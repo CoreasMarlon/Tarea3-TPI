36f4ccd843dc3b400ae9fc0efbc51254b84ce041
CoreasMarlon <marlon.coreasv@gmail.com>
2020-11-02 22:26:14 -0600

    ordenamiento por nombre ASC y DESC

--- models/Usuario.php
+++ models/Usuario.php
@@ -133,7 +133,7 @@
     
       private function crateSqlSort($rules) {
         $sql = "";
-        $fields = ['id', 'municipio', 'departamento']; // set available filters here
+        $fields = ['id', 'name']; // set available filters here
         if (count($rules)) {
           $i = 0;
           foreach ($rules as $key => $value) {
@@ -147,13 +147,10 @@
                 case 'id':
                   if ( $value == 'ASC' || $value == 'DESC' ) $sql .= " u.idUsuario " . $value ." "; 
                   break;
-                case 'municipio':
-                  if ( $value == 'ASC' || $value == 'DESC' ) $sql .= " u.municipio" . $value ." "; 
+                case 'name':
+                  if ( $value == 'ASC' || $value == 'DESC' ) $sql .= " u.nombre " . $value ." "; 
                   break;
-                case 'departamento':
-                  if ( $value == 'ASC' || $value == 'DESC' ) $sql .= " u.departamento " . $value ." "; 
-                  break;
-                
+
                 default:
                   # code...
                   break;
@@ -164,50 +161,5 @@
         }
         return $sql;
       }
-    
-      /*
-      public function get()
-      {
-        $sql = "SELECT * FROM " . self::TABLE_NAME . " WHERE id_donante = " . $this->id_donante;
-        if ($result = $this->db->query($sql, MYSQLI_USE_RESULT)) {
-          $data = array();
-          while ($obj = $result->fetch_object()) {
-            array_push($data, $obj);
-          }
-          return $data;
-        }
-      }
-    
-      public function create()
-      {
-        $sql = "INSERT INTO " . self::TABLE_NAME . " (nombre_donante,apellido_donante,estado_donante,id_departamento,id_municipio,id_sangre,prueba_donante,telefono_donante,carnet,historial) VALUES 
-        ('" . $this->getNombre_donante() . "','" . $this->apellido_donante . "','Created'," . $this->id_departamento . ","  . $this->id_municipio . "," . $this->id_sangre . ",'" . $this->prueba_donante . "','" . $this->telefono_donante . "','" . $this->carnet . "','" . $this->historial . "')";
-        $result = false;
-        if (!$result = $this->db->query($sql)) {
-          return "Falló la creación del registro: (" . $this->db->errno . ") " . $this->db->error;
-        }
-        return $result;
-      }
-    
-      public function update()
-      {
-        $sql = "UPDATE " . self::TABLE_NAME . " SET estado_donante = '" . $this->estado_donante . "' WHERE id_donante = " . $this->id_donante;
-        echo $sql . "<br>";
-        if (!$result = $this->db->query($sql)) {
-          return "Falló la actualizacion del registro: (" . $this->db->errno . ") " . $this->db->error;
-        }
-        return $result;
-      }
-    
-      public function delete()
-      {
-        $sql = "DELETE FROM " . self::TABLE_NAME . " where id_donante = " . $this->id_donante;
-        echo $sql . "<br>";
-        if (!$result = $this->db->query($sql)) {
-          return "Falló la creación del registro: (" . $this->db->errno . ") " . $this->db->error;
-        }
-        return $result;
-      }
-      */
     }
 ?>
