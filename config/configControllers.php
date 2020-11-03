35c3a8397468964274ffc76718756a881430b7f7
adolfo <saguzman315@gmail.com>
2020-11-02 21:41:17 -0600

    Mi nueva rama

--- config/configControllers.php
+++ config/configControllers.php
@@ -1,7 +1,7 @@
 <?php 
     require_once "database/MySqlConnection.php";
 
-    define ('BASE_DIR', "http://localhost/Tarea3-TPI/");
+    define ('BASE_DIR', "http://localhost:8080/Tarea3-TPI/");
     define ('CONTROLLERS_DIR', "controllers/");
     define ('DEFAULT_CONTROLLER', "Home");
     define ('DEFAULT_ACTION', "showHome");
