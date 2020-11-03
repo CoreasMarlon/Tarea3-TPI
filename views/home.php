55d44c733d58e7951502ed5fc8c2980c63d1b316
adolfo <saguzman315@gmail.com>
2020-11-02 21:56:39 -0600

    Cambio Marlon

--- views/home.php
+++ views/home.php
@@ -1,9 +1,4 @@
 <main>
-<<<<<<< HEAD
     <a href="<?=BASE_DIR?>Usuario/list"><h1>View Users Hola mundo</h1></a>
+    
 </main>
-=======
-    <a href="<?=BASE_DIR?>Usuario/list"><h1>View Users</h1></a>
-    <p>Alv</p>
-</main>
->>>>>>> f8530c26380f0c3381ef5aae2199984b0e845f09
